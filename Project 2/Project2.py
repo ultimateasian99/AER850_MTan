4aa050a9adedef5f412f346ce737c67a20e856fe
ultimateasian99 <millietan36@gmail.com>
2024-11-08 01:01:14 -0500

    project update

--- Project 2/Project2.py
+++ Project 2/Project2.py
@@ -5,13 +5,15 @@ import numpy as np
 
 
 # tensorflow 2.18 Keras 3.6
-from keras._tf_keras.keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense, Input 
+from keras._tf_keras.keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense 
 from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
 #from tensorflow import keras
 from keras import Sequential
 import tensorflow as tf
-
+from keras._tf_keras.keras.optimizers import Adam
+from keras._tf_keras.keras.regularizers import l2
+from keras._tf_keras.keras.callbacks import ReduceLROnPlateau
 
 #STEP 1 DATA PROCESSING 
 
@@ -35,7 +37,7 @@ else:
 train_datagen = ImageDataGenerator(
     rescale = 1./255,
     shear_range = 0.2, 
-    zoom_range = 0.2, 
+    zoom_range = 0.2
 )
 
 test_datagen = ImageDataGenerator(
@@ -46,7 +48,6 @@ test_datagen = ImageDataGenerator(
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size = (img_height, img_width),
-    color_mode = 'grayscale',
     batch_size = batchsize,
     shuffle = True,
     class_mode = 'categorical'
@@ -61,7 +62,6 @@ valid_datagen = ImageDataGenerator(
 valid_generator = valid_datagen.flow_from_directory(
     valid_data_dir,
     target_size = (img_height, img_width),
-    color_mode = 'grayscale',
     batch_size = batchsize,
     shuffle = True,
     class_mode = 'categorical'
@@ -72,8 +72,8 @@ valid_generator = valid_datagen.flow_from_directory(
 model = Sequential()
 
 #feature learning layers of NN architecure
-model.add(Input(shape=input_shape))
-model.add(Conv2D(32,(3,3),activation='relu'))
+#model.add(Input(shape=input_shape))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))  
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Conv2D(64,(3,3),activation='relu'))
@@ -85,14 +85,15 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 #classification layers of NN architecture
 model.add(Flatten())                              # convert to 1d vector
 
-model.add(Dense(16,activation='elu'))
-model.add(Dropout(0.5))
+model.add(Dense(32,activation='relu',kernel_regularizer=l2(0.0001)))           
+model.add(Dropout(0.3))
 model.add(Dense(3,activation='softmax'))
+
 
 #compile model
 model.compile(
-    optimizer = 'adam',                              # adapative moment estimation
-    loss = 'categorical_crossentropy',              # for classification problems
+    optimizer = Adam(0.001),          
+    loss = 'categorical_crossentropy',             
     metrics = ['accuracy']
 )
 print(model.summary())
@@ -100,12 +101,36 @@ print(model.summary())
 #training mi model
 history = model.fit(
     train_generator,
-    epochs = 50,
+    epochs = 10,                    #originally 50
     validation_data=valid_generator
 )
 
-#STEP 3 HYPERPARAMETER ANALYSIS
+#save the entire model as a .keras zip archive
+model.save('mymodel.keras')
 
 
+#Plotting time
+train_acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
+epoch_range = range(len(train_acc))
 
 #STEP 4 MODEL EVALUATION
+plt.figure()
+plt.subplot(1,2,1)
+plt.plot(epoch_range,train_acc, label = "Training Accuracy")
+plt.plot(epoch_range,val_acc, label = "Validation Accuracy")
+plt.legend(loc = "lower right")
+plt.title("Training and Validation Accuracy")
+
+plt.subplot(1,2,2)
+plt.plot(epoch_range,train_loss, label = "Training Loss")
+plt.plot(epoch_range,val_loss, label = "Validation Loss")
+plt.legend(loc = "upper right")
+plt.title("Training and Validation Loss")
+
+plt.show()
+
+
+
